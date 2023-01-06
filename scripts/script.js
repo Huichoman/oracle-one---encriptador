4691b2296c5d890e0dd3599ded16c605e2b7ddc1
Huichoman <rockbotica@gmail.com>
2023-01-05 19:18:18 -0600

    Lógica terminada

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,56 @@
-// alert("Hola mundo");
+let inputText = "input";
+let outputText = "output";
+const inputTextElement = document.getElementById("inputTextArea");
+const outputTextElement = document.getElementById("outputTextArea");
+
+String.prototype.map = function (func) {
+  let stringArray = this.split("");
+
+  let newStringArray = stringArray.map((item, index) => {
+    return func.call(window, item, index, this);
+  });
+
+  return newStringArray.join("");
+};
+
+function encriptar() {
+  let buffer = "";
+  inputText = inputTextElement.value;
+  outputText = inputText.map((e) => {
+    switch (e) {
+      case "a":
+        buffer = "ai";
+        break;
+      case "e":
+        buffer = "enter";
+        break;
+      case "i":
+        buffer = "imes";
+        break;
+      case "o":
+        buffer = "ober";
+        break;
+      case "u":
+        buffer = "ufat";
+        break;
+      default:
+        buffer = e;
+        break;
+    }
+    return buffer;
+  });
+
+  outputTextElement.value = outputText;
+}
+
+function desencriptar() {
+  let buffer = "";
+  inputText = inputTextElement.value;
+  outputText = inputText.replaceAll("ai", "a");
+  outputText = outputText.replaceAll("enter", "e");
+  outputText = outputText.replaceAll("imes", "i");
+  outputText = outputText.replaceAll("ober", "o");
+  outputText = outputText.replaceAll("ufat", "u");
+
+  outputTextElement.value = outputText;
+}
